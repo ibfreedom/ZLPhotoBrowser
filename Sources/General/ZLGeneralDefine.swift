58a73b575ed82e7c0897c0c75148121715f02e59
iferret's <wanggang3@staff.sina.com.cn>
2023-02-03 20:28:00 +0800

    update => 关闭调试

--- Sources/General/ZLGeneralDefine.swift
+++ Sources/General/ZLGeneralDefine.swift
@@ -187,13 +187,13 @@ func ZLMainAsync(after: TimeInterval = 0, handler: @escaping (() -> Void)) {
 
 func zl_debugPrint(_ message: Any...) {
 //    message.forEach { debugPrint($0) }
-    #if DEBUG
-    print(message.map { "\($0)" }.joined(separator: " "))
-    #endif
+//    #if DEBUG
+//    print(message.map { "\($0)" }.joined(separator: " "))
+//    #endif
 }
 
 func zlLoggerInDebug(_ lastMessage: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
-    #if DEBUG
-        print("\(file):\(line): \(lastMessage())")
-    #endif
+//    #if DEBUG
+//        print("\(file):\(line): \(lastMessage())")
+//    #endif
 }
